8ed88a6a76c8a27a0e7eb9e37131a18892ff10c1
yuriy82791 <yuriy82791@gmail.com>
2020-04-14 21:44:26 +0300

    Updates

---
+++ precache-manifest.11eeeed9949de8007d782355c16127c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f45d6089eea0c244793aac6b98db92",
+    "url": "/site-app/index.html"
+  },
+  {
+    "revision": "9afc950548672820ab49",
+    "url": "/site-app/static/css/main.e9087e46.chunk.css"
+  },
+  {
+    "revision": "aafbca3be85d38f5cbea",
+    "url": "/site-app/static/js/2.05c4384d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site-app/static/js/2.05c4384d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afc950548672820ab49",
+    "url": "/site-app/static/js/main.387ace48.chunk.js"
+  },
+  {
+    "revision": "abec69c52e88b7adc89d",
+    "url": "/site-app/static/js/runtime-main.5d1e58dd.js"
+  },
+  {
+    "revision": "6bb4d0b39b66bef9dea1bec6fecb4d13",
+    "url": "/site-app/static/media/image.6bb4d0b3.webp"
+  }
+]);
